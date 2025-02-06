022b81fcee5f11e1427664b0ad48685afc4d508e
nadiachaja <nadiagaja13@gmail.com>
2025-02-06 12:04:28 +0100

    javascript koppelen

---
+++ public/scripts/client.js
@@ -0,0 +1,10 @@
+// selecteer de id van de button 
+// addEventlistener toevoegen
+// function maken 
+// if en else 
+
+
+
+const make = document.getElementById('camera');
+
+make.addEventListener
--- public/scripts/script.js
+++
@@ -1,9 +0,0 @@
-
-const flipButton = document.querySelector('button')
-const card = document.querySelector('article')
-
-flipButton.addEventListener('click', flipper)
-
-function flipper() {
-  card.classList.toggle('flip')
-}
